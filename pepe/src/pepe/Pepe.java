915cbacf280df033f2196cf6a6b8fd856ffa6485
Miguel <Miguel@DESKTOP-E6PMOJ0>
2021-05-23 19:50:44 +0200

    Primera version de Pepe

---
+++ pepe/src/pepe/Pepe.java
@@ -0,0 +1,39 @@
+package pepe;
+
+/**
+ * @author Miguel
+ *
+ */
+public class Pepe {
+	
+	/**
+	 * @param pepe
+	 * @param pepe1
+	 * @author Miguel
+	 * @return devuelve el nombre
+	 */
+
+	
+	private String pepe;
+	
+	private String pepe1;
+
+	
+	
+	public String getPepe() {
+		return pepe;
+	}
+
+	public void setPepe(String pepe) {
+		this.pepe = pepe;
+	}
+	
+	public String getPepe1() {
+		return pepe1;
+	}
+
+	public void setPepe1(String pepe1) {
+		this.pepe1 = pepe1;
+	}
+
+}
